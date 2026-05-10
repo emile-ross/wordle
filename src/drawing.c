c528cd932ce37d28f295a624c7c7e5ab8fe1588b
imawsumm <imawsumm@users.noreply.github.com>
2026-05-09 23:06:01 -0400

    feat: add drawing logic
    also update wordle bin

--- src/drawing.c
+++ src/drawing.c
@@ -9,6 +9,23 @@ void drawing(char wordle_answer[INDEX_LETTERS_WORD])
 	}
 	wordle_answer = wordle_answer_temp;
 
+	bool x_pattern false;
+
+	char all_answers[5][ALLWORDS];
+	if (!x_pattern)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			char filtered_temp_arr[NUM_ALL_WORDS];
+			for (int j = 0; i < NUM_ALL_WORDS; i++)
+			{
+				if ()
+				{
+				}
+			}
+		}
+	}
+	
 	printf("%s\n", wordle_answer);
 }
 
