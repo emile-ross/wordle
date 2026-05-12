b5e1387c5a6c94caaa2fb6d5e4c24b4259f2ff96
imawsumm <imawsumm@users.noreply.github.com>
2026-05-12 18:31:36 -0400

    fix: parse through filtered_array
    the program was previously parsing through all words instead of the
    filtered array it needed to parse through the filtered array in order to
    work properly

--- src/drawing.c
+++ src/drawing.c
@@ -5,22 +5,40 @@ void drawing(char wordle_answer[INDEX_LETTERS_WORD], bool x_pattern)
 	char wordle_answer_temp[INDEX_LETTERS_WORD];
 	for (int i = 0; i < NUM_LETTERS_WORD; i++)
 	{
+		/* convert the wordle answer to uppercase for easier parsing */
 		wordle_answer_temp[i] = (char)to_uppercase(wordle_answer[i]);
 	}
+
 	wordle_answer = wordle_answer_temp;
 
 	char all_answers[INDEX_LETTERS_WORD][NUM_ALL_WORDS][NUM_LETTERS_WORD];
 
 	if (!x_pattern)
 	{
-		for (int letter_index = 0; letter_index < NUM_LETTERS_WORD; letter_index++)
+		for (int entry_i = 0; entry_i < NUM_LETTERS_WORD; entry_i++)
 		{
+			char grey_letters[5];
+			char green_letter;
+			int green_letter_index = 0;
+			for (int i = 0; i < NUM_LETTERS_WORD; i++)
+			{
+				if (i == entry_i)
+				{
+					green_letter = wordle_answer[i];
+				}
+				else
+				{
+					grey_letters[green_letter_index] = wordle_answer[i];
+					green_letter_index++;
+				}
+			}
+
 			char filtered_words[NUM_ALL_WORDS][INDEX_LETTERS_WORD];
 			char filtered_words_temp[NUM_ALL_WORDS][INDEX_LETTERS_WORD];
 			int num_answers = 0;
 			for (int j = 0; j < NUM_ALL_WORDS; j++)
 			{
-				if (wordle_answer[letter_index] == all_words[j][letter_index])
+				if (green_letter == all_words[j][entry_i])
 				{
 					strcpy(filtered_words_temp[num_answers], all_words[j]);
 					num_answers++;
@@ -29,41 +47,40 @@ void drawing(char wordle_answer[INDEX_LETTERS_WORD], bool x_pattern)
 			int temp_count = 0;
 			for (int word_iteration = 0; word_iteration < num_answers; word_iteration++)
 			{
-				bool letter_matches = false;
+				bool all_letters_are_grey = true;
 				/* loop for every letter in the original word (answer) */
 				for (int answer_letter_index = 0; answer_letter_index < NUM_LETTERS_WORD; answer_letter_index++)
 				{
 					/* if the letter in the orignal word matches the index of the temporary word to check,
 					 * we have to go to the next letter */
-					if (answer_letter_index != letter_index)
+					if (answer_letter_index != entry_i)
 					{
-						char original_letter = wordle_answer[answer_letter_index];
 						for (int letter_iteration = 0; letter_iteration < NUM_LETTERS_WORD; letter_iteration++)
 						{
-							if (original_letter == all_words[word_iteration][letter_iteration])
+							if (grey_letters[answer_letter_index] == filtered_words_temp[word_iteration][letter_iteration])
 							{
-								letter_matches = true;
+								all_letters_are_grey = false;
 								/* break out of the answer_letter_index loop 
 								 * since a letter is matching in the word */
 								break; 
 							}
 						}
 
-						if (letter_matches)
+						if (!all_letters_are_grey)
 						{
 							break;
 						}
 					}
 				}
 			
-				if (!letter_matches)
+				if (all_letters_are_grey)
 				{
 					strcpy(filtered_words[temp_count], filtered_words_temp[word_iteration]);
 					temp_count++;
 				}
 			}
 			num_answers = temp_count;
-			int user_letter_index = letter_index + 1;
+			int user_letter_index = entry_i + 1;
 			printf(BOLD_S"\nEntry %d \n"STYLE_END, user_letter_index);
 
 			if (num_answers < 1)
@@ -73,7 +90,7 @@ void drawing(char wordle_answer[INDEX_LETTERS_WORD], bool x_pattern)
 
 			for (int j = 0; j < num_answers; j++)
 			{
-				strcpy(all_answers[letter_index][j], filtered_words[j]);
+				strcpy(all_answers[entry_i][j], filtered_words[j]);
 			}
 			print_as_table(table_width, num_answers, awsum_table_mode, filtered_words);
 		}
