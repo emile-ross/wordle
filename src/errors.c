d2b34a16dfab27d59b5e54166903d978325d1ead
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-05-25 17:00:08 -0400

    feat: add indenting support
    indents the number of lines the user specified in the config file

--- src/errors.c
+++ src/errors.c
@@ -4,47 +4,52 @@ void err(int error_code)
 {
 	bool critical = true;
 
-    switch (error_code)
-    {
-		case 1:
-		    printf("Missing arguments\n");
-		    break;
-		    
-		case 5:
-		    printf("Index value must be between 1 and 5 (inclusive)\n");
-		    break;
+	switch (error_code)
+    	{
+	case 1:
+		printf("Missing arguments");
+		break;
+		
+	case 5:
+		printf("Index value must be between 1 and 5 (inclusive)");
+		break;
+		
+	case 7:
+		printf("This feature hasn't been implemented");
+		break;
+		
+	case 10:
+		printf("The only valid flags are:\n\n");
+		printf(BOLD_S" -e"STYLE_END" (exclude)\n");
+		printf(BOLD_S" -x"STYLE_END" (exclude)\n");
+		printf(BOLD_S"   --excludes"STYLE_END" (the letter \'X\' isn't at position Y)\n");
+		printf(BOLD_S" -s"STYLE_END" (strict)\n");
+		printf(BOLD_S"   --strict"STYLE_END" (the letter \'X\' is at position Y)\n\n");
+		printf(BOLD_S" -a"STYLE_END" (letter is absent)\n");
+		printf(BOLD_S"   --absent"STYLE_END" (the letter \'X\' isn't in the word)\n");
+		printf(BOLD_S" -i"STYLE_END" (includes)\n");
+		printf(BOLD_S"   --includes"STYLE_END" (the letter \'X\' is in the word)");
+		break;
+		
+	case 15:
+		printf("Unknown word list");
+		break;
+		
+	case 20:
+		printf(BOLD_S"No possible answers.\n"STYLE_END);
+		printf(BOLD_S"Try editing your filters."STYLE_END);
+		break;
+	
+	default:
+		printf("Missing error message\n");
+		critical = false;
+		break;
+    	}
 
-		case 7:
-		    printf("This feature hasn't been implemented\n");
-		    break;
-
-		case 10:
-		    printf("The only valid flags are:\n\n");
-		    printf(BOLD_S" -e"STYLE_END" (exclude)\n");
-		    printf(BOLD_S" -x"STYLE_END" (exclude)\n");
-		    printf(BOLD_S"   --excludes"STYLE_END" (the letter \'X\' isn't at position Y)\n");
-		    printf(BOLD_S" -s"STYLE_END" (strict)\n");
-		    printf(BOLD_S"   --strict"STYLE_END" (the letter \'X\' is at position Y)\n\n");
-		    printf(BOLD_S" -a"STYLE_END" (letter is absent)\n");
-		    printf(BOLD_S"   --absent"STYLE_END" (the letter \'X\' isn't in the word)\n");
-		    printf(BOLD_S" -i"STYLE_END" (includes)\n");
-		    printf(BOLD_S"   --includes"STYLE_END" (the letter \'X\' is in the word)\n\n");
-		    break;
-
-		case 15:
-		    printf("Unknown word list\n");
-		    break;
-
-		case 20:
-		    printf(BOLD_S"No possible answers.\n"STYLE_END);
-		    printf(BOLD_S"Try editing your filters.\n"STYLE_END);
-		    break;
-
-		default:
-		    printf("Missing error message\n");
-			critical = false;
-		    break;
-    }
+	for (int i = 0; i < indenting; i++)
+    	{
+    	        printf("\n");
+    	}
 
 	if (critical)
 	{
@@ -76,6 +81,7 @@ void warn(warnings warning_type)
 		default: 
 			printf("Unknown warning\n");
 			critical = true;
+			err((int)warning_type);
 			break;
 	}
 
@@ -114,5 +120,8 @@ void warn(warnings warning_type)
 
 	getchar();
 
-	printf("\n");
+	for (int i = 0; i < indenting; i++)
+	{
+		printf("\n");
+	}
 }
