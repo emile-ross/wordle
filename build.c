39ea8eed1ffb78b447871db6f8da229970ac15d9
emile-ross <emileross@tutanota.de>
2026-06-10 03:07:11 -0400

    build: add latin compilation
    compile the latin word list

--- build.c
+++ build.c
@@ -4,14 +4,14 @@
 #include <stdbool.h>
 
 #define compiler_name_len (8)
-#define num_src_files (12)
+#define num_src_files (13)
 
 char *CLANG_COMPILER_NAME = "clang";
 char *ZIG_COMPILER_NAME = "zig cc";
 char *GCC_COMPILER_NAME = "gcc";
 
-#define fp_size 32
-#define base_args 1
+#define fp_size (32)
+#define base_args (1)
 
 char *all_src_files[num_src_files] = 
 {
@@ -27,6 +27,7 @@ char *all_src_files[num_src_files] =
 	"src/word-lists/common_words",
 	"src/word-lists/nyt_words",
 	"src/word-lists/fr_all_words",
+	"src/word-lists/la_all_words",
 };
 
 char *src_file_extention = ".c";
