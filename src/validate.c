101b5e78241287309358b1fa08f99a9057cc9e56
emile-ross <emileross@tutanota.de>
2026-06-11 15:00:38 -0400

    refactor: move validate_words() to validate.c
    easier to read and easier to find the function

---
+++ src/validate.c
@@ -0,0 +1,102 @@
+#include "header.h"
+
+void validate_word(char command_word_string[INDEX_LETTERS_WORD])
+{
+	char command_word_string_temp[INDEX_LETTERS_WORD];
+
+	for (int i = 0; i < NUM_LETTERS_WORD; i++)
+	{
+		command_word_string_temp[i] = to_uppercase(command_word_string[i]);
+	}
+
+	strcpy(command_word_string, command_word_string_temp);
+
+	int num_matches = 0;
+	bool word_list_matches[NUM_WORD_LISTS];
+	char *word_list_names[NUM_WORD_LISTS];
+
+	word_list_names[nyt]	= "New-York Times word list";
+	word_list_names[common]	= "the \"common words\" list";
+	word_list_names[all]	= "\"all words\" list";
+
+	word_list_names[fr_all]	= "french \"all words\" list";
+	word_list_names[la_all]	= "latin \"all words\" list";
+
+	for (int i = 0; i < NUM_WORD_LISTS; i++)
+	{
+		word_list_matches[i] = false;
+	}
+
+	if (command_word_string != NULL)
+	{
+		for (int i = 0; i < NUM_WORD_LISTS; i++)
+		{
+			char (*ptr)[INDEX_LETTERS_WORD];
+
+
+			int num_words = 0;
+			/* TODO: make a single function to do this instead of multiple duplicate switches in parsing.c and here */
+
+			num_words = list_match((ALL_WORD_LISTS)i, &ptr);
+	
+			for (int j = 0; j < num_words; j++)
+			{
+				if (strcmp(command_word_string, ptr[j]) == 0)
+				{
+					word_list_matches[i] = true;
+					num_matches++;
+					break;
+				}
+			}
+		}
+
+	}
+
+	printf(BOLD_S"\nThe word: "UDRL_S"%s\n"STYLE_END, command_word_string);
+
+	if (num_matches > 0)
+	{
+		printf("was found in the following lists:\n\n");
+		if (word_list_matches[nyt])
+		{
+			printf(ANSI_GREEN"New-York Times word list\n"STYLE_END);
+		}
+		if (word_list_matches[common])
+		{
+			printf(ANSI_GREEN"the \"common words\" list\n"STYLE_END);
+		}
+
+		if (word_list_matches[all])
+		{
+			printf(ANSI_GREEN"\"all words\" list\n"STYLE_END);
+		}
+
+		if (word_list_matches[fr_all])
+		{
+			if (word_list_matches[all])
+			{
+				printf("\n");
+			}
+			printf(ANSI_GREEN"french \"all words\" list\n"STYLE_END);
+		}
+
+		if (word_list_matches[la_all])
+		{
+			if (!word_list_matches[fr_all])
+			{
+				if (word_list_matches[all])
+				{
+					printf("\n");
+				}
+			}
+
+			printf(ANSI_GREEN"latin \"all words\" list\n"STYLE_END);
+		}
+	}
+	else
+	{
+		printf("was not found in any word list\n");
+	}
+	exit(0);
+}
+
