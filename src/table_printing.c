6e6ae922aa18d514e8de5f301c9b08ab41164bbb
emile-ross <emileross@tutanota.de>
2026-09-15 22:07:01 -0400

    feat: table_printing.c: make use of safe_write()
    writes to the percent string using the safe_write() function

--- src/table_printing.c
+++ src/table_printing.c
@@ -89,19 +89,12 @@ void print_as_table(uint16_t width, uint64_t total_elements, bool awsum_mode, ch
 
 			if (remaining_percent < 0.05F)
 			{
-				int ret = 1 + snprintf(percent, size, "%.4f", remaining_percent);
-				if (ret > (signed)size)
-				{
-					if (ret < 0)
-						err(ZERO_SIZED_BUF);
-
-					percent = realloc(percent, (unsigned)ret);
-					size = (unsigned)ret;
-				}
+				percent = safe_write(percent, &size, "%.4f", remaining_percent);
 			}
+
 		}
 
-		printf(BOLD_S"%d possible words (%f%% of initial words)\n"STYLE_END, n_possible_answers, remaining_percent);
+		printf(BOLD_S"%d possible words (%s%% of initial words)\n"STYLE_END, n_possible_answers, percent);
 
 		/* initial_words is initialised in src/parsing.c in the parsing() fn */
 		printf("(Narrowed down from %d possible words)\n", initial_words);
